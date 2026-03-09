5651f8b74d3bc51368e81e58da0500b6affed8e7
Emīls Smirnovs <Smirnovs@lvt.local>
2026-03-09 16:12:06 +0200

    Izveidoti ciklu un nosacījuma sekcijas

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 console.log("JS strādā!")
 
-document.querySelector("#tn-variables").addEventListener("Click", () => {
+document.querySelector("#btn-variables").addEventListener("click", () => {
     let vards = "Intars"
-    let vecums = 17;
+    let vecums = 17
+    const nacionalitate = "Latvijā"
+    document.querySelector("#variables-output").textContent = `${vards} ir ${vecums} gadus vecs un ir dzimis ${nacionalitate}.`
+})
+
+document.querySelector("#btn-age").addEventListener("click", () => {
+    const age = document.querySelector("#age-input").value
+    let tiesibas = document.querySelector("#tiesibas-check").checked
+    let text = age >= 18 && tiesibas ? "Atļauts" : "Aizliegts" // Ternārais operators
+    document.querySelector("#age-output").textContent = text
+})
+
+document.querySelector("#btn-loop").addEventListener("click", () => {
+    let output= ""
+    for (let i = 1; i <= 20; i++) output += i + " "
+    document.querySelector("#loop-output").textContent = output
+
+    let output2= ""
+    do{
+        output2 += i + ""
+        i++
+    }while (i >= izveletais)
 })
