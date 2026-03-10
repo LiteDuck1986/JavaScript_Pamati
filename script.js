403e39296a9e7b35e18382132271ff9eabd169c2
Emīls Smirnovs <Smirnovs@lvt.local>
2026-03-10 10:58:44 +0200

    Jaunas sekcijas un JS.

--- script.js
+++ script.js
@@ -59,3 +59,130 @@ document.querySelector("#btn-loop3").addEventListener("click", () => {
     }
     document.querySelector("#loop-output3").textContent = output
 })
+
+let draugi = ["Edijs", "Intars", "Džons", "Kārlis"]
+
+document.querySelector("#btn-array").addEventListener("click", () => {
+    // draugi.push("Andrejs") // masīva beigās pievieno jaunu elementu
+
+    document.querySelector("#array-output").textContent = draugi.join(", ")
+
+})
+
+document.querySelector("#btn-array2").addEventListener("click", () => {
+
+    let izveletais = document.querySelector("#name-input").value
+    let irDraugs = draugi.includes(izveletais)
+    const index = draugi.indexOf(izveletais)
+    const element = document.getElementById("#remove-friend");
+
+    if(irDraugs){
+        document.querySelector("#array-output2").textContent = "Jā masīvā ir draugs " + izveletais + "."
+        return
+    }
+    else if(!irDraugs){
+        document.querySelector("#array-output2").textContent = "Masīvā nav tāds draugs " + "'" +izveletais + "'."
+        return
+    }
+
+    document.querySelector("#array-output").textContent = draugi.join(", ")
+
+})
+
+document.querySelector("#btn-objects").addEventListener("click", () => {
+    let persona = {
+        vards: "Pēteris", // Īpašības
+        vecums: 18,
+        nacionalitate: "Latvietis",
+        sveiciens(){ // metode
+            return "Mani sauc " + this.vards + ", man ir " + this.vecums + " gadi un esmu " + this.nacionalitate + "."
+        }
+    }
+    document.querySelector("#objects-output").textContent = persona.sveiciens()
+})
+
+// function aprekinatLaukumu(platums, augstums){
+//     return platums * augstums
+// }
+
+const aprekinatLaukumu = (platums, augstums) => platums * augstums // 2. Variants
+
+document.querySelector("#btn-function").addEventListener("click", () => {
+    const w = Number(document.querySelector("#width").value)
+    const h = Number(document.querySelector("#height").value)
+    const output = document.querySelector("#function-output")
+
+    if(w && h){
+        output.textContent = `Aprēķinātais laukums: ${aprekinatLaukumu(w, h)}`
+    }else{
+        output.textContent = "Ievadi abus skaitļus!"
+    }
+})
+
+document.querySelector("#btn-color").addEventListener("click", () => {
+    const color = "#" + Math.floor(Math.random()* 16777215).toString(16)
+    box.textContent = color
+    box.style.background = color
+})
+
+document.querySelector("#btn-countdown").addEventListener("click", () => {
+    const countdownOut = document.querySelector("#countdown-output")
+    const dateInput = document.querySelector("#date-input").value
+
+    if(!dateInput){
+        countdownOut.textContent = "Izvēlies datumu!"
+        return
+    }
+
+    const today = new Date() // Šodienas datums
+    const targetDate = new Date(dateInput)
+
+    const diff = targetDate - today
+    if(diff < 0){
+        countdownOut.textContent = "Šis nav nākotnes datums!"
+        return
+    }
+
+    const daysLeft = Math.ceil(diff / (1000 * 60 * 60 * 24))
+
+    if(daysLeft === 1){
+        countdownOut.textContent = "Atlikusi 1 diena!"
+    }else{
+        countdownOut.textContent = `Atlikušas ${daysLeft} dienas līdz ${targetDate.toLocaleDateString("lv-LV")}`
+    }
+})
+
+// 9. Sekcija
+const box1 = document.querySelector("#box1")
+const box2 = document.querySelector("#box2")
+const eventOut = document.querySelector("#event-output")
+
+box1.addEventListener("mouseenter", () => {
+    eventOut.textContent = "Kursors uzlikts uz kastes #1"
+})
+
+box1.addEventListener("mouseleave", () => {
+    eventOut.textContent = "Kursors izgāja no kaste #1"
+})
+
+box2.addEventListener("click", () => {
+    eventOut.textContent = "Uzklikšķināji uz kaste #2"
+})
+
+box2.addEventListener("click", () => {
+    eventOut.textContent = "Uzklikšķināji uz kaste #2"
+})
+
+document.addEventListener("keydown", (e) =>{
+    eventOut.textContent = `Nospiedi taustiņu: ${e.key}`
+})
+
+window.addEventListener("scroll", () =>{
+    eventOut.textContent = "Lapa tiek ritināta..."
+
+    let scrollTimer
+
+    scrollTImer = setTimeout(() =>{
+        eventOut.textContent = "Lapa vairs netiek ritināta"
+    }, 500)
+})
